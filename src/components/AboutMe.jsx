d853105819ce83e978a24b4ed7a7076196c9b62b
Lorenzo Macias <lzomacias@gmail.com>
2025-05-14 14:52:01 -0400

    about me

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -14,7 +14,7 @@ function AboutMe() {
 
         <p>
           where i went: In <a href='https://zicklin.baruch.cuny.edu/' 
-          className='aboutmelinks' target="_blank" rel="noopener noreferrer">college</a> I studied Computer Information Systems and Marketing. I got a job as a sales rep at the small corporate firm 
+          className='aboutmelinks' target="_blank" rel="noopener noreferrer">college</a> I studied Computer Information Systems and Marketing. I got a job as a sales rep at the small corporate law firm 
           <a href='https://thelawyerjames.com' 
           className='aboutmelinks' target="_blank" rel="noopener noreferrer"> The Lawyer James </a> 
           and concurrently interned with several other tech companies.  Most notably 
@@ -23,17 +23,15 @@ function AboutMe() {
           where I spent 4 months selling their POS product to NYC business owners. Upon graduation I became a certified Financial Advisor at 
           <a href='https://www.masterworks.com/' 
           className='aboutmelinks' target="_blank" rel="noopener noreferrer"> Masterworks </a> 
-          and raised ~$150,000(1 million in LFV) in investment for a securitized art product on a 6-month retainer.
+          and raised ~$200,000(1 million in LFV) in investment for a securitized art product on a 6-month retainer.
         </p>
 
         <p>
-          where i am today: In March of 2025 I graduated the Full-Stack Web Development Bootcamp, where I developed a demo online bookstore called 
-          <a href='https://github.com/lzo-macias/CareerSimulationBookBuddy.git' 
-          className='aboutmelinks' target="_blank" rel="noopener noreferrer"> BookBuddy </a> 
-          (frontend working with API) and was team lead on the new social media app 
+          where i am today: In March of 2025 I graduated the Full-Stack Web Development Bootcamp, where I was team lead on the new social media app 
           <a href='https://shenanigram.netlify.app/' 
           className='aboutmelinks' target="_blank" rel="noopener noreferrer"> Shenanigram </a> 
-          (fullstack). I am most passionate about building projects that not only present well visually but follow development principles for security and organization. For fun I solve puzzles on AlgoExpert, cook, and on occasion maybe even a nightclub if my friends put me up to it.
+          (fullstack).  I then continued honing my skills building           <a href='https://coolpeoplenyc.netlify.app/' 
+          className='aboutmelinks' target="_blank" rel="noopener noreferrer"> CoolPeople </a> which uses scrapers and LLM's to track local politics in NYC. I am most passionate about building projects that not only present well visually but follow development principles for security and organization. For fun I solve puzzles on AlgoExpert, cook, and on occasion maybe even a nightclub if my friends put me up to it.
         </p>
     </div>
     </Element>
