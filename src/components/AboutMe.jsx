e1b87c2a70afa0377ced30bcd55ef732de3e9adc
Lorenzo Macias <lzomacias@gmail.com>
2025-06-03 16:42:52 -0400

    fixes

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -14,24 +14,24 @@ function AboutMe() {
 
         <p>
           where i went: In <a href='https://zicklin.baruch.cuny.edu/' 
-          className='aboutmelinks' target="_blank" rel="noopener noreferrer">college</a> I studied Computer Information Systems and Marketing. I got a job as a Customer Success Manager at the small corporate law firm 
+          className='aboutmelinks' target="_blank" rel="noopener noreferrer">college</a> I studied Computer Information Systems and Marketing and later became a Customer Success Manager at the corporate law firm 
           <a href='https://thelawyerjames.com' 
-          className='aboutmelinks' target="_blank" rel="noopener noreferrer"> The Lawyer James </a> 
-          and concurrently interned with several other tech companies.  Most notably 
+          className='aboutmelinks' target="_blank" rel="noopener noreferrer"> The Lawyer James,  </a> 
+          generating 2M dollars in revenue and implementing a refferal and review program producing 100+ reviews and 25% increased SEO perfomance. I also concurrently interned as a Solutions Engineer at 
           <a href='https://www.linkedin.com/company/wowpayments/' 
           className='aboutmelinks' target="_blank" rel="noopener noreferrer"> Wow Payments </a> 
-          where I spent 4 months selling their POS product to NYC business owners. Upon graduation I became a certified Financial Advisor and Solutions Engineer at 
+          where I spent two months selling their POS product to NYC business owners, generating ~$3,000 in monthly recurring revenue. Upon graduation I became a certified Financial Advisor and Solutions Engineer at 
           <a href='https://www.masterworks.com/' 
           className='aboutmelinks' target="_blank" rel="noopener noreferrer"> Masterworks </a> 
-          and raised ~$200,000(1 million in LFV) in investment for a securitized art product on a 6-month retainer.
+          and raised 1M in lifetime value for Masterworks and $200,000 in day one investment for a securitized art product on a 6-month retainer.
         </p>
 
         <p>
           where i am today: In March of 2025 I graduated the Full-Stack Web Development Bootcamp, where I was team lead on the new social media app 
           <a href='https://shenanigram.netlify.app/' 
           className='aboutmelinks' target="_blank" rel="noopener noreferrer"> Shenanigram </a> 
-          (fullstack).  I then continued honing my skills building           <a href='https://coolpeoplenyc.netlify.app/' 
-          className='aboutmelinks' target="_blank" rel="noopener noreferrer"> CoolPeople </a> which uses scrapers and Ai to track local politics in NYC. I am most passionate about building projects that not only present well visually but follow development principles for security and organization. For fun I solve puzzles on AlgoExpert, cook, and on occasion maybe even a nightclub if my friends put me up to it.
+          (fullstack).  And am now presently building            <a href='https://coolpeoplenyc.netlify.app/' 
+          className='aboutmelinks' target="_blank" rel="noopener noreferrer"> CoolPeople </a> , a civic voting tool which uses scrapers and Ai to track local politics in NYC. I am most passionate when I get to combine my knack for client managment and people skills with my technical expertise. 
         </p>
     </div>
     </Element>
