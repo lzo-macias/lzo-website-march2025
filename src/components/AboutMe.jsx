3184b4e22a0c2c61187bcb06bc4f42bd15ebbb41
Lorenzo Macias <lzomacias@gmail.com>
2025-06-03 15:03:17 -0400

    updates

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -14,13 +14,13 @@ function AboutMe() {
 
         <p>
           where i went: In <a href='https://zicklin.baruch.cuny.edu/' 
-          className='aboutmelinks' target="_blank" rel="noopener noreferrer">college</a> I studied Computer Information Systems and Marketing. I got a job as a sales rep at the small corporate law firm 
+          className='aboutmelinks' target="_blank" rel="noopener noreferrer">college</a> I studied Computer Information Systems and Marketing. I got a job as a Customer Success Manager at the small corporate law firm 
           <a href='https://thelawyerjames.com' 
           className='aboutmelinks' target="_blank" rel="noopener noreferrer"> The Lawyer James </a> 
           and concurrently interned with several other tech companies.  Most notably 
           <a href='https://www.linkedin.com/company/wowpayments/' 
           className='aboutmelinks' target="_blank" rel="noopener noreferrer"> Wow Payments </a> 
-          where I spent 4 months selling their POS product to NYC business owners. Upon graduation I became a certified Financial Advisor at 
+          where I spent 4 months selling their POS product to NYC business owners. Upon graduation I became a certified Financial Advisor and Solutions Engineer at 
           <a href='https://www.masterworks.com/' 
           className='aboutmelinks' target="_blank" rel="noopener noreferrer"> Masterworks </a> 
           and raised ~$200,000(1 million in LFV) in investment for a securitized art product on a 6-month retainer.
@@ -31,7 +31,7 @@ function AboutMe() {
           <a href='https://shenanigram.netlify.app/' 
           className='aboutmelinks' target="_blank" rel="noopener noreferrer"> Shenanigram </a> 
           (fullstack).  I then continued honing my skills building           <a href='https://coolpeoplenyc.netlify.app/' 
-          className='aboutmelinks' target="_blank" rel="noopener noreferrer"> CoolPeople </a> which uses scrapers and LLM's to track local politics in NYC. I am most passionate about building projects that not only present well visually but follow development principles for security and organization. For fun I solve puzzles on AlgoExpert, cook, and on occasion maybe even a nightclub if my friends put me up to it.
+          className='aboutmelinks' target="_blank" rel="noopener noreferrer"> CoolPeople </a> which uses scrapers and Ai to track local politics in NYC. I am most passionate about building projects that not only present well visually but follow development principles for security and organization. For fun I solve puzzles on AlgoExpert, cook, and on occasion maybe even a nightclub if my friends put me up to it.
         </p>
     </div>
     </Element>
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -19,7 +19,7 @@ function Experience() {
             <div className='date'>2024 6 month retainer</div>
             <div className='text_section'>
               <h3>Masterworks <span className='position'>• Financial Advisor</span></h3>
-              <p>Called rich people and sold them fractional investment in blue-chip artworks by artists like Picasso and Monet. Resulted in ~200,000 in investment and an estimated ~$1,000,000 in LFV for Masterworks. Top performer in cohort of advisors.</p>
+              <p>Called rich people and sold them fractional investment in blue-chip artworks by artists like Picasso and Monet. Explainned a complicated technical platform for alternative investing.  Resulted in ~200,000 in investment and an estimated ~$1,000,000 in LFV for Masterworks. Top performer in cohort of advisors.</p>
               <div className='tags'>
                 <span>Financial Advisor</span>
                 <span>Outbound Sales</span>
@@ -42,7 +42,7 @@ function Experience() {
             <div className='date'>2022-2024</div>
             <div className='text_section'>
               <h3>The Lawyer James <span className='position'>• Sales Representative</span></h3>
-              <p>Called business owners and real estate agents to grow and maintain the book of business at The Lawyer James. My two years at the firm resulted in upwards of $2,000,000 in additional revenue. I also implemented a review and referral program driving new business.</p>
+              <p>Called business owners and real estate agents to grow and maintain the book of business at The Lawyer James. My two years at the firm resulted in upwards of $2,000,000 in additional revenue. I also implemented a review and referral program driving 100+ Google reviews and improving SEO performance by 25% .</p>
               <div className='tags'>
                 <span>Corporate Law</span>
                 <span>Sales</span>
@@ -87,7 +87,7 @@ function Experience() {
             <div className='date'>2023 four month retainer</div>
             <div className='text_section'>
               <h3>Wow Payments <span className='position'>• Sales Representative</span></h3>
-              <p>Sold Point-of-sale payment software systems to a large portfolio of Spanish and English speaking B2B prospects resulting in 4 closed deals generating ~$3000 of dollars in monthly recurring revenue. Managed lead generation, qualification, nurturing, and closes for a geographic sales territory spanning 15 NYC square miles.</p>
+              <p>Sold Point-of-sale payment software systems to a large portfolio of Spanish and English speaking B2B prospects resulting in 4 closed deals generating ~$3000 of dollars in monthly recurring revenue. Managed lead generation, qualification, nurturing, and closes for a geographic sales territory spanning 15 NYC square miles.  And acted as a technical liaison, communicating integration needs between clients and the development team to enhance onboarding</p>
               <div className='tags'>
                 <span>Outbound Sales</span>
                 <span>Spanish</span>
